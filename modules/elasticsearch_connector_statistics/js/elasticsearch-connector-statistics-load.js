7f78e36da7d14ea81b724130145469af9cfe63c4
Nikolay Ignatov <nikolai.ignatov@gmail.com>
2014-03-12 17:50:08 +0200

    Initial and working version of statistics module.

---
+++ modules/elasticsearch_connector_statistics/js/elasticsearch-connector-statistics-load.js
@@ -0,0 +1,15 @@
+(function ($) {
+  var img = $("<img />").attr('src', Drupal.settings.elasticsearch_connector.statistics.image_src)
+    .load(function() {
+      if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+        if (typeof console == "object") {
+          console.log("Problem loading the statistics image.");
+        }
+      }
+      else {
+        if (typeof console == "object") {
+          console.log("Statistics image has been loaded successfully.");
+        }
+      }
+  });
+})(jQuery);
