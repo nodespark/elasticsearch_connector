0b2979e6c4b6149722798dd5d1f31f5057b41902
Nikolay Ignatov <nikolai.ignatov@gmail.com>
2014-05-29 13:01:06 +0300

    Building the backbone strucure for development of version 8.x.

---
+++ lib/Drupal/elasticsearch_connector/Controller/ElasticsearchConnectorController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\elasticsearch_connector\Controller\ElasticsearchConnectorController
+ */
+
+namespace Drupal\elasticsearch_connector\Controller;
+
+/**
+ * Example page controller.
+ */
+class ElasticsearchConnectorController {
+  /**
+   * Generates an example page.
+   */
+  public function adminClusters() {
+    return array(
+      '#markup' => t('TODO: Build the required logic here!'),
+    );
+  }
+
+}
+?>
