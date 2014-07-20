581910302e06b3df7c0c1eac6068eb4e08886607
Prateek Sachan <ps@prateeksachan.com>
2014-07-20 15:29:16 +0530

    Adding links for indices

--- src/Controller/ElasticsearchController.php
+++ src/Controller/ElasticsearchController.php
@@ -146,5 +146,59 @@ class ElasticSearchController extends ControllerBase {
     return $output;
   }
 
+  /**
+   * Display all indices in cluster.
+   *
+   * @param object
+   * @return array
+   */
+  public function elasticsearchClusterIndices(ConfigEntityBase $elasticsearch_cluster) {
+    $headers = array(
+      array('data' => t('Index name')),
+      array('data' => t('Docs')),
+      array('data' => t('Size')),
+      array('data' => t('Operations')),
+    );
+
+    $rows = array();
+    $cluster_info = Cluster::getClusterInfo($elasticsearch_cluster);
+    $client = $cluster_info['client'];
+
+    if ($client && !empty($cluster_info['info']) && Cluster::checkClusterStatus($cluster_info['info'])) {
+      $indices = $client->indices()->stats();
+      foreach ($indices['indices'] as $index_name => $index_info) {
+        $row = array();
+
+        $operations = theme('links', array(
+          'links' => array(
+            array('title' => t('Aliases'), 'href' => 'admin/config/search/elasticsearch/clusters/' . $elasticsearch_cluster->cluster_id . '/indices/' . $index_name . '/aliases'),
+            array('title' => t('Delete'), 'href' => 'admin/config/search/elasticsearch/clusters/' . $elasticsearch_cluster->cluster_id . '/indices/' . $index_name . '/delete'),
+          ),
+          'attributes' => array(
+            'class' => array('links', 'inline'),
+          ),
+        ));
+
+        $row[] = $index_name;
+        $row[] = $index_info['total']['docs']['count'];
+        $row[] = format_size($index_info['total']['store']['size_in_bytes']);
+        $row[] = $operations;
+
+        $rows[] = $row;
+      }
+    }
+    else {
+      drupal_set_message(t('The cluster cannot be connected for some reason.'), 'error');
+    }
+
+    $output['elasticsearch']['table'] = array(
+      '#theme' => 'table',
+      '#header' => $headers,
+      '#rows' => $rows,
+      '#attributes' => array('class' => array('admin-elasticsearch-indices')),
+    );
+
+    return $output;
+  }
 
 }
--- src/Entity/Index.php
+++ src/Entity/Index.php
@@ -6,6 +6,7 @@ use Drupal\Core\Config\Entity\ConfigEntityBase;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Elasticsearch\Client;
 use Drupal\Component\Utility\UrlHelper;
+use Drupal\elasticsearch\Entity\Cluster;
 
 /**
  * Defines the search server configuration entity.
@@ -46,61 +47,5 @@ class Index extends ConfigEntityBase {
   public $num_of_shards;
 
   public $num_of_replica;
-
-  /**
-   * Display all indices in cluster.
-   *
-   * @param object
-   * @return array
-   */
-  function elasticsearchClusterIndices($cluster) {
-    echo 'hi';
-    $headers = array(
-      array('data' => t('Index name')),
-      array('data' => t('Docs')),
-      array('data' => t('Size')),
-      array('data' => t('Operations')),
-    );
-
-    $rows = array();
-    $cluster_info = getClusterInfo($cluster);
-    $client = $cluster_info['client'];
-
-    if ($client && !empty($cluster_info['info']) && checkClusterStatus($cluster_info['info'])) {
-      $indices = $client->indices()->stats();
-      foreach ($indices['indices'] as $index_name => $index_info) {
-        $row = array();
-
-        $operations = theme('links', array(
-          'links' => array(
-            array('title' => t('Aliases'), 'href' => 'admin/config/search/elasticsearch/clusters/' . $cluster->cluster_id . '/indices/' . $index_name . '/aliases'),
-            array('title' => t('Delete'), 'href' => 'admin/config/search/elasticsearch/clusters/' . $cluster->cluster_id . '/indices/' . $index_name . '/delete'),
-          ),
-          'attributes' => array(
-            'class' => array('links', 'inline'),
-          ),
-        ));
-
-        $row[] = $index_name;
-        $row[] = $index_info['total']['docs']['count'];
-        $row[] = format_size($index_info['total']['store']['size_in_bytes']);
-        $row[] = $operations;
-
-        $rows[] = $row;
-      }
-    }
-    else {
-      drupal_set_message(t('The cluster cannot be connected for some reason.'), 'error');
-    }
-
-    $output['elasticsearch']['table'] = array(
-      '#theme' => 'table',
-      '#header' => $headers,
-      '#rows' => $rows,
-      '#attributes' => array('class' => array('admin-elasticsearch-indices')),
-    );
-
-    return $output;
-  }
 
 }
