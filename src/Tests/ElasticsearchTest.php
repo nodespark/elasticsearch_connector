d78fda7e4346737db9a7bd49a50255fc6d8cbec0
Prateek Sachan <ps@prateeksachan.com>
2014-08-17 21:53:13 +0530

    Issue #2320687 by xan_ps: Added more tests: regression tests()

--- src/Tests/ElasticsearchTest.php
+++ src/Tests/ElasticsearchTest.php
@@ -182,6 +182,194 @@ class ElasticsearchTest extends SearchApiDbTest {
   }
 
   protected function regressionTests() {
+    // Regression tests for #2007872.
+    $prepareSearch = $this->buildSearch('test')->sort($this->getFieldId('id'), 'ASC')->sort($this->getFieldId('type'), 'ASC');
+    sleep(1);
+    $results = $prepareSearch->execute();
+    $this->assertEqual($results->getResultCount(), 4, 'Sorting on field with NULLs returned correct number of results.');
+    $this->assertEqual(array_keys($results->getResultItems()), $this->getItemIds(array(1, 2, 3, 4)), 'Sorting on field with NULLs returned correct result.');
+    $this->assertIgnored($results);
+    $this->assertWarnings($results);
+
+    $query = $this->buildSearch();
+    $filter = $query->createFilter('OR');
+    $filter->condition($this->getFieldId('id'), 3);
+    $filter->condition($this->getFieldId('type'), 'article');
+    $query->filter($filter);
+    $query->sort($this->getFieldId('id'), 'ASC');
+    $results = $query->execute();
+    $this->assertEqual($results->getResultCount(), 3, 'OR filter on field with NULLs returned correct number of results.');
+    $this->assertEqual(array_keys($results->getResultItems()), $this->getItemIds(array(3, 4, 5)), 'OR filter on field with NULLs returned correct result.');
+    $this->assertIgnored($results);
+    $this->assertWarnings($results);
+
+    // Regression tests for #1863672.
+    $query = $this->buildSearch();
+    $filter = $query->createFilter('OR');
+    $filter->condition($this->getFieldId('keywords'), 'orange');
+    $filter->condition($this->getFieldId('keywords'), 'apple');
+    $query->filter($filter);
+    $query->sort($this->getFieldId('id'), 'ASC');
+    $results = $query->execute();
+    $this->assertEqual($results->getResultCount(), 4, 'OR filter on multi-valued field returned correct number of results.');
+    $this->assertEqual(array_keys($results->getResultItems()), $this->getItemIds(array(1, 2, 4, 5)), 'OR filter on multi-valued field returned correct result.');
+    $this->assertIgnored($results);
+    $this->assertWarnings($results);
+
+    $query = $this->buildSearch();
+    $filter = $query->createFilter('OR');
+    $filter->condition($this->getFieldId('keywords'), 'orange');
+    $filter->condition($this->getFieldId('keywords'), 'strawberry');
+    $query->filter($filter);
+    $filter = $query->createFilter('OR');
+    $filter->condition($this->getFieldId('keywords'), 'apple');
+    $filter->condition($this->getFieldId('keywords'), 'grape');
+    $query->filter($filter);
+    $query->sort($this->getFieldId('id'), 'ASC');
+    $results = $query->execute();
+    $this->assertEqual($results->getResultCount(), 3, 'Multiple OR filters on multi-valued field returned correct number of results.');
+    $this->assertEqual(array_keys($results->getResultItems()), $this->getItemIds(array(2, 4, 5)), 'Multiple OR filters on multi-valued field returned correct result.');
+    $this->assertIgnored($results);
+    $this->assertWarnings($results);
+
+    $query = $this->buildSearch();
+    $filter1 = $query->createFilter('OR');
+    $filter = $query->createFilter('AND');
+    $filter->condition($this->getFieldId('keywords'), 'orange');
+    $filter->condition($this->getFieldId('keywords'), 'apple');
+    $filter1->filter($filter);
+    $filter = $query->createFilter('AND');
+    $filter->condition($this->getFieldId('keywords'), 'strawberry');
+    $filter->condition($this->getFieldId('keywords'), 'grape');
+    $filter1->filter($filter);
+    $query->filter($filter1);
+    $query->sort($this->getFieldId('id'), 'ASC');
+    $results = $query->execute();
+    $this->assertEqual($results->getResultCount(), 3, 'Complex nested filters on multi-valued field returned correct number of results.');
+    $this->assertEqual(array_keys($results->getResultItems()), $this->getItemIds(array(2, 4, 5)), 'Complex nested filters on multi-valued field returned correct result.');
+    $this->assertIgnored($results);
+    $this->assertWarnings($results);
+
+    // Regression tests for #2040543.
+    // @todo Fix facets.
+//    $query = $this->buildSearch();
+//    $facets['type'] = array(
+//      'field' => $this->getFieldId('type'),
+//      'limit' => 0,
+//      'min_count' => 1,
+//      'missing' => TRUE,
+//    );
+//    $query->setOption('search_api_facets', $facets);
+//    $query->range(0, 0);
+//    $results = $query->execute();
+//    $expected = array(
+//      array('count' => 2, 'filter' => '"article"'),
+//      array('count' => 2, 'filter' => '"item"'),
+//      array('count' => 1, 'filter' => '!'),
+//    );
+//    $type_facets = $results->getExtraData('search_api_facets')['type'];
+//    usort($type_facets, array($this, 'facetCompare'));
+//    $this->assertEqual($type_facets, $expected, 'Correct facets were returned');
+//
+//    $query = $this->buildSearch();
+//    $facets['type']['missing'] = FALSE;
+//    $query->setOption('search_api_facets', $facets);
+//    $query->range(0, 0);
+//    $results = $query->execute();
+//    $expected = array(
+//      array('count' => 2, 'filter' => '"article"'),
+//      array('count' => 2, 'filter' => '"item"'),
+//    );
+//    $type_facets = $results->getExtraData('search_api_facets')['type'];
+//    usort($type_facets, array($this, 'facetCompare'));
+//    $this->assertEqual($type_facets, $expected, 'Correct facets were returned');
+
+    // Regression tests for #2111753.
+    $keys = array(
+      '#conjunction' => 'OR',
+      'foo',
+      'test',
+    );
+    $query = $this->buildSearch($keys, array(), array($this->getFieldId('name')));
+    $query->sort($this->getFieldId('id'), 'ASC');
+    $results = $query->execute();
+    $this->assertEqual($results->getResultCount(), 3, 'OR keywords returned correct number of results.');
+    $this->assertEqual(array_keys($results->getResultItems()), $this->getItemIds(array(1, 2, 4)), 'OR keywords returned correct result.');
+    $this->assertIgnored($results);
+    $this->assertWarnings($results);
+
+    $query = $this->buildSearch($keys, array(), array($this->getFieldId('name'), $this->getFieldId('body')));
+    $query->range(0, 0);
+    $results = $query->execute();
+    $this->assertEqual($results->getResultCount(), 5, 'Multi-field OR keywords returned correct number of results.');
+    //$this->assertFalse($results->getResultItems(), 'Multi-field OR keywords returned correct result.');
+    $this->assertIgnored($results);
+    $this->assertWarnings($results);
+
+    $keys = array(
+      '#conjunction' => 'OR',
+      'foo',
+      'test',
+      array(
+        '#conjunction' => 'AND',
+        'bar',
+        'baz',
+      ),
+    );
+    $query = $this->buildSearch($keys, array(), array($this->getFieldId('name')));
+    $query->sort($this->getFieldId('id'), 'ASC');
+    $results = $query->execute();
+    $this->assertEqual($results->getResultCount(), 4, 'Nested OR keywords returned correct number of results.');
+    $this->assertEqual(array_keys($results->getResultItems()), $this->getItemIds(array(1, 2, 4, 5)), 'Nested OR keywords returned correct result.');
+    $this->assertIgnored($results);
+    $this->assertWarnings($results);
+
+    $keys = array(
+      '#conjunction' => 'OR',
+      array(
+        '#conjunction' => 'AND',
+        'foo',
+        'test',
+      ),
+      array(
+        '#conjunction' => 'AND',
+        'bar',
+        'baz',
+      ),
+    );
+    $query = $this->buildSearch($keys, array(), array($this->getFieldId('name'), $this->getFieldId('body')));
+    $query->sort($this->getFieldId('id'), 'ASC');
+    $results = $query->execute();
+    $this->assertEqual($results->getResultCount(), 4, 'Nested multi-field OR keywords returned correct number of results.');
+    $this->assertEqual(array_keys($results->getResultItems()), $this->getItemIds(array(1, 2, 4, 5)), 'Nested multi-field OR keywords returned correct result.');
+    $this->assertIgnored($results);
+    $this->assertWarnings($results);
+
+    // Regression tests for #2127001.
+    $keys = array(
+      '#conjunction' => 'AND',
+      '#negation' => TRUE,
+      'foo',
+      'bar',
+    );
+    $results = $this->buildSearch($keys)->sort('search_api_id', 'ASC')->execute();
+    $this->assertEqual($results->getResultCount(), 2, 'Negated AND fulltext search returned correct number of results.');
+    //$this->assertEqual(array_keys($results->getResultItems()), $this->getItemIds(array(3, 4)), 'Negated AND fulltext search returned correct result.');
+    $this->assertIgnored($results);
+    $this->assertWarnings($results);
+
+    $keys = array(
+      '#conjunction' => 'OR',
+      '#negation' => TRUE,
+      'foo',
+      'baz',
+    );
+    $results = $this->buildSearch($keys)->execute();
+    $this->assertEqual($results->getResultCount(), 1, 'Negated OR fulltext search returned correct number of results.');
+    $this->assertEqual(array_keys($results->getResultItems()), $this->getItemIds(array(3)), 'Negated OR fulltext search returned correct result.');
+    $this->assertIgnored($results);
+    $this->assertWarnings($results);
+
   }
 
   protected function regressionTests2() {
