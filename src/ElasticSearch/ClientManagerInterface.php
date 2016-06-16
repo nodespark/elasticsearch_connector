bfafd57632e998a9dc482ea38113a3379fc44c70
Lachezar Valchev <lachezar.valchev@ffwagency.com>
2016-06-16 22:50:22 +0300

    NO ISSUE by lachezar.valchev: As DESConnector ClientFactory is now passed through the service container I have changed the ClientManager interface to reflect that change. It is now expecting just the ClientFactoryInterface.

--- src/ElasticSearch/ClientManagerInterface.php
+++ src/ElasticSearch/ClientManagerInterface.php
@@ -5,9 +5,11 @@ namespace Drupal\elasticsearch_connector\ElasticSearch;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 // TODO: Cluster should be an interface!
 use Drupal\elasticsearch_connector\Entity\Cluster;
+use nodespark\DESConnector\ClientFactoryInterface;
+
 
 interface ClientManagerInterface {
-  public function __construct(ModuleHandlerInterface $module_handler, $clientManagerClass);
+  public function __construct(ModuleHandlerInterface $module_handler, ClientFactoryInterface $clientManagerFactory);
 
   /**
    * @param \Drupal\elasticsearch_connector\Entity\Cluster $cluster
