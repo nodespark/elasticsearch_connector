4e326224b20c55781b06f5c1e502ffc19de5175f
Juampy <juampynr@gmail.com>
2017-10-09 18:47:39 +0200

    Add script to check coding standards

---
+++ .circleci/code-sniffer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex
+
+# Runs CodeSniffer checks on a Drupal module.
+
+robo setup:skeleton
+
+robo add:coding-standards-deps
+
+robo add:modules $1
+
+robo update:dependencies
+
+# Install dependencies and configure phpcs
+vendor/bin/phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer
+
+# Check coding standards
+mkdir -p /tmp/phpcs
+vendor/bin/phpcs --standard=Drupal --report=junit modules/$1 > /tmp/phpcs/phpcs.xml
