a1113dce023cc6eb5bc05d023b0720fe21c531ee
d.novikov <d.novikov@478976.no-reply.drupal.org>
2018-02-07 21:51:49 +0200

    Issue #2879848 by sedax, d.novikov: Added BETWEEN and NOT BETWEEN

--- src/ElasticSearch/Parameters/Factory/FilterFactory.php
+++ src/ElasticSearch/Parameters/Factory/FilterFactory.php
@@ -121,6 +121,36 @@ class FilterFactory {
           ];
           break;
 
+        case 'BETWEEN':
+          $filter = [
+            'range' => [
+              $condition->getField() => [
+                'from' => (!empty($condition->getValue()[0])) ? $condition->getValue()[0] : NULL,
+                'to' => (!empty($condition->getValue()[1])) ? $condition->getValue()[1] : NULL,
+                'include_lower' => FALSE,
+                'include_upper' => FALSE,
+              ],
+            ],
+          ];
+          break;
+
+        case 'NOT BETWEEN':
+          $filter = [
+            'bool' => [
+              'must_not' => [
+                'range' => [
+                  $condition->getField() => [
+                    'from' => (!empty($condition->getValue()[0])) ? $condition->getValue()[0] : NULL,
+                    'to' => (!empty($condition->getValue()[1])) ? $condition->getValue()[1] : NULL,
+                    'include_lower' => FALSE,
+                    'include_upper' => FALSE,
+                  ],
+                ],
+              ]
+            ]
+          ];
+          break;
+
         default:
           throw new \Exception('Undefined operator ' . $condition->getOperator() . ' for ' . $condition->getField() . ' field! Incorrect filter criteria is using for searching!');
       }
